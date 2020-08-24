42c3273308facec16d9fc5bf3bb71d326d84255e
Owen Martin <martin.owen22@gmail.com>
2020-08-23 20:56:24 -0600

    Actually save movie

--- Simulation.py
+++ Simulation.py
@@ -104,7 +104,7 @@ class Simulation:
             self.r_or_u,
             now
         ))
-        anim.save('data/anim_{}agents_{}x{}_k={}_steps={}_{}distribution{}_movie.mp4'.format(
+        anim.save('data/anim_{}agents_{}x{}_k={}_steps={}_{}distribution{}_movie.mov'.format(
             self.total_agents,
             self.n, self.n,
             self.coupling_strength,
