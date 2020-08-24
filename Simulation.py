e5e3ae307b4137f84f200a113c3b78580e2f383a
Owen Martin <martin.owen22@gmail.com>
2020-08-23 21:02:52 -0600

    cant save .mov

--- Simulation.py
+++ Simulation.py
@@ -94,17 +94,9 @@ class Simulation:
         ax.set_ylabel('Y')
 
         anim = FuncAnimation(fig, animate, frames=self.steps, fargs=(self.agent_array, firefly_paths),
-                             interval=250, blit=False)
+                             interval=100, blit=False)
         now = datetime.now()
         anim.save('data/anim_{}agents_{}x{}_k={}_steps={}_{}distribution{}_gif.gif'.format(
-            self.total_agents,
-            self.n, self.n,
-            self.coupling_strength,
-            self.steps,
-            self.r_or_u,
-            now
-        ))
-        anim.save('data/anim_{}agents_{}x{}_k={}_steps={}_{}distribution{}_movie.mov'.format(
             self.total_agents,
             self.n, self.n,
             self.coupling_strength,
